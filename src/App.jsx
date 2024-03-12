d219f8a748fc0a0222e65ea579d09435f369a1c6
Gedaias Jair <gedaias@geda.dev>
2024-03-11 20:57:15 -0600

    Create header
    Add new entry button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./styles/App.css";
 import ExpensesView from "./components/ExpensesView";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <ExpensesView />
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import "../styles/header.css";
+
+function Header() {
+  return (
+    <header>
+      <button className="button">New Entry</button>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
