8398a638f3620cfca3419cdae119985d296745e7
Gedaias Jair <gedaias@geda.dev>
2024-03-12 21:30:51 -0600

    Add state to toggle the modal
    Move the header, input and select css selectors to App

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
-import "../styles/header.css";
+import { useState } from "react";
+import EntryModal from "./EntryModal";
 
 function Header() {
+  const [activeModal, setActiveModal] = useState(false);
+
+  const toggleModal = () => setActiveModal(!activeModal);
+
   return (
     <header>
-      <button className="button">New Entry</button>
+      <button className="button" onClick={toggleModal}>
+        New Entry
+      </button>
       <div></div>
+      {activeModal && <EntryModal />}
     </header>
   );
 }
