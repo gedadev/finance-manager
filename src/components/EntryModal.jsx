cf39b4ccc2a5b1b8e3a085ba8bb08d43a7b8ea1f
Gedaias Jair <gedaias@geda.dev>
2024-03-14 20:34:55 -0600

    Add state to handle the data of the form
    Create function to update this data when is modified in the inputs

--- src/components/EntryModal.jsx
+++ src/components/EntryModal.jsx
@@ -5,7 +5,27 @@ import axios from "axios";
 import InputSelect from "./InputSelect";
 
 function EntryModal({ toggleModal }) {
+  // move this function to context as is shared with FilterOptions
+  const convertToDate = (timestamp) => {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month =
+      date.getMonth() < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1;
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+    return `${year}-${month}-${day}`;
+  };
+
   const [inputOptions, setInputOptions] = useState({});
+  const [formData, setFormData] = useState({
+    date: convertToDate(Date.now()),
+    payMethod: "",
+    category: "",
+    subcategory: "",
+    recurrent: "no",
+    store: "",
+    item: "",
+    price: "",
+  });
 
   useEffect(() => {
     const getFilters = async () => {
@@ -23,55 +43,94 @@ function EntryModal({ toggleModal }) {
     getFilters();
   }, []);
 
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    const update = { ...formData, [name]: value };
+    setFormData(update);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
     <div className="entry-modal">
-      <form action="" className="entry-form">
+      <form action="" className="entry-form" onSubmit={handleSubmit}>
         <div className="input-container">
           <label htmlFor="">Date:</label>
-          <input type="date" name="" id="" />
+          <input
+            type="date"
+            name="date"
+            id=""
+            onChange={handleInput}
+            value={formData.date}
+          />
         </div>
         <div className="input-container">
           <label htmlFor="">Pay Method:</label>
-          <select name="">
+          <select name="payMethod" onChange={handleInput}>
             <InputSelect inputOptions={inputOptions.payMethod} />
           </select>
         </div>
         <div className="input-container">
           <label htmlFor="">Category:</label>
-          <select name="">
+          <select name="category" onChange={handleInput}>
             <InputSelect inputOptions={inputOptions.category} />
           </select>
         </div>
         <div className="input-container">
           <label htmlFor="">Subcategory:</label>
-          <select name="">
+          <select name="subcategory" onChange={handleInput}>
             <InputSelect inputOptions={inputOptions.subcategory} />
           </select>
         </div>
         <div className="input-container">
           <label htmlFor="">Recurrent:</label>
-          <select name="">
+          <select
+            name="recurrent"
+            onChange={handleInput}
+            value={formData.recurrent}
+          >
             <option value="no">No</option>
             <option value="yes">Yes</option>
           </select>
         </div>
         <div className="input-container">
           <label htmlFor="">Store:</label>
-          <input type="text" name="" id="" />
+          <input
+            type="text"
+            name="store"
+            id=""
+            onChange={handleInput}
+            value={formData.store}
+          />
         </div>
         <div className="input-container">
           <label htmlFor="">Item:</label>
-          <input type="text" name="" id="" />
+          <input
+            type="text"
+            name="item"
+            id=""
+            onChange={handleInput}
+            value={formData.item}
+          />
         </div>
         <div className="input-container">
           <label htmlFor="">Price:</label>
-          <input type="number" name="" id="" />
+          <input
+            type="number"
+            name="price"
+            id=""
+            onChange={handleInput}
+            value={formData.price}
+          />
         </div>
         <div className="form-buttons">
           <button type="submit" className="button">
             Add Entry
           </button>
-          <button type="submit" className="button" onClick={toggleModal}>
+          <button type="button" className="button" onClick={toggleModal}>
             Discard
           </button>
         </div>
